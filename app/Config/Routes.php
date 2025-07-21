2da8f82b82c280324cf75336f19e1372dc38ef35
Pichitchai Seejampa <pichitchai6174@gmail.com>
2025-07-21 14:51:53 +0700

    เพิ่ม route validateToken สำหรับการตรวจสอบ Token ใน API การยืนยันตัวตน

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,4 +11,5 @@ $routes->group('api/auth', function ($routes) {
     $routes->post('login', 'AuthController::login');
     $routes->post('refresh', 'AuthController::refresh');
     $routes->post('logout', 'AuthController::logout');
+    $routes->post('validate', 'AuthController::validateToken');
 });
